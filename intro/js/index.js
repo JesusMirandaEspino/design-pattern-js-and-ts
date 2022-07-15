a02a2c314891cff083fcf01c6257f7fbbd771c2a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-14 21:15:39 -0500

    funciones

--- intro/js/index.js
+++ intro/js/index.js
@@ -1,1 +1,28 @@
-console.log('Hola mundo');
+
+// Funcion de primer orden
+function sum( a, b ){
+    return a + b;
+}
+
+let res = sum( 1,2 );
+console.log( res );
+
+const fSuma = sum;
+res = fSuma(5, 6);
+console.log(res);
+
+// Funcion de orden superior
+function superior(fn, a, b){
+    console.log('Hace algo');
+    console.log(fn(a,b));
+}
+
+superior( sum, 10, 20 );
+
+
+// Arrow function
+superior( (a,b)=>a*b, 10, 20 );
+superior( (a,b)=>{
+    let c = a + b;
+    return c * 2;
+}, 10, 20 );
