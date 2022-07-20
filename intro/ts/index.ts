73cff92d805cf23b13869a6a4d4bc35455e37d4f
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-19 20:55:37 -0500

    clases typescript

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -1,1 +1,20 @@
 console.log('Hola mundo');
+
+class Bebida{
+
+    private name: string;
+
+    constructor(name: string){  
+        this.name = name;
+    }
+
+    info(): string{
+        return `El nombre de la bebida es ${this.name}`;
+    }
+
+}
+
+
+const bebida1 = new Bebida('Soda');
+
+console.log( bebida1.info() );
