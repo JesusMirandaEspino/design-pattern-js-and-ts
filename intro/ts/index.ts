e865f5cdcf81a718821ad757cb2b3e257a744f3b
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-19 21:24:00 -0500

    interfaces

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -1,5 +1,11 @@
 console.log('Hola mundo');
 
+interface Product{
+    price: number;
+    getPrice(): string;
+}
+
+
 class Bebida{
 
     private name: string;
@@ -15,23 +21,56 @@ class Bebida{
 }
 
 
-class Bebida2 extends Bebida{
+class Bebida2 extends Bebida implements Product{
 
     private sugar: number;
+    public price: number;
 
-    constructor(name: string, sugar: number){  
+    constructor(name: string, sugar: number, price: number){  
         super(name);
         this.sugar = sugar;
+        this.price = price;
     }
     
     info(): string{
         return super.info() + `, el nivel de azucar es: ${this.sugar}`;
     }
 
+    getPrice(): string{
+        return `El precio es ${this.price}`;
+    }   
+
+}
+
+
+class Snack implements Product{
+    name: string;
+    price: number;
+
+    constructor( name: string,  price: number ){        
+        this.name = name;
+        this.price = price;
+    }
+    getPrice(): string{
+        return `El precio es ${this.price}`;
+    }
 }
 
 const bebida1 = new Bebida('Soda');
-const bebida2 = new Bebida2('coke', 9.5);
+const bebida2 = new Bebida2('coke', 9.5, 15);
 console.log( bebida1.info() );
 console.log( bebida2.info() );
 
+
+const productos: Product[] = [ new Bebida2( 'mutzi', 10, 25 ), new Bebida2( 'power rage', 5, 30 ), new Bebida2( 'hope life', 3, 29 )
+    ];
+
+
+function  getPricesAll(productosList: Product[]){
+    productosList.forEach( producto => {
+        console.log( producto.getPrice() );
+    });
+}
+
+
+getPricesAll( productos );
