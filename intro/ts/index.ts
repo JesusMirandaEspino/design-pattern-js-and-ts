44dfb9c2ff5f130e0335c0ece5149058594d76d1
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-19 21:01:19 -0500

    herencia typescript

--- intro/ts/index.ts
+++ intro/ts/index.ts
@@ -15,6 +15,23 @@ class Bebida{
 }
 
 
-const bebida1 = new Bebida('Soda');
+class Bebida2 extends Bebida{
 
+    private sugar: number;
+
+    constructor(name: string, sugar: number){  
+        super(name);
+        this.sugar = sugar;
+    }
+    
+    info(): string{
+        return super.info() + `, el nivel de azucar es: ${this.sugar}`;
+    }
+
+}
+
+const bebida1 = new Bebida('Soda');
+const bebida2 = new Bebida2('coke', 9.5);
 console.log( bebida1.info() );
+console.log( bebida2.info() );
+
