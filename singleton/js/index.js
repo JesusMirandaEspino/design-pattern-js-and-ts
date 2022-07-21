1a23385c244b761c9ae1aa39fd480807287a495f
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-21 09:15:17 -0500

    singleton part2

--- singleton/js/index.js
+++ singleton/js/index.js
@@ -22,3 +22,30 @@ console.log( singleton2.ramdon );
 console.log( singleton3.ramdon );
 
 console.log( singleton1.ramdon === singleton2.ramdon );
+
+
+class Weekdays{
+    dayEs = [ 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+    dayEn = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    constructor(lang){
+        this.lang = lang;
+
+        if(Weekdays.instance){
+            return Weekdays.instance
+        }
+
+        Weekdays.instance = this;
+
+    }
+
+    getDays(){
+        return this.lang  === 'Es' ? this.dayEs : this.dayEn;
+    }
+}
+
+const Week1 = new Weekdays('Es');
+const Week2 = new Weekdays();
+
+console.log(Week1.getDays());
+console.log(Week2.getDays());
