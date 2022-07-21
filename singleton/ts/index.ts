34773320ad20173ecce8f375faf514769dd132a7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-21 14:54:53 -0500

    singleton ts

---
+++ singleton/ts/index.ts
@@ -0,0 +1,19 @@
+class SingletonTs{
+
+    private static instance: SingletonTs;
+    public random: number;
+
+    private constructor(){
+        this.random = Math.random();
+    }
+
+    public static getInstance(): SingletonTs{
+        if( !this.instance ){
+            this.instance = new SingletonTs();
+        }
+        return this.instance;
+    }
+}
+
+
+const newSingleton = SingletonTs.getInstance();
