6578d68e536e8f950617b2ade21b26c88054cb31
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-21 14:57:27 -0500

    singleton ts parte 2

--- singleton/ts/index.ts
+++ singleton/ts/index.ts
@@ -16,4 +16,13 @@ class SingletonTs{
 }
 
 
-const newSingleton = SingletonTs.getInstance();
+const newSingleton1 = SingletonTs.getInstance();
+const newSingleton2 = SingletonTs.getInstance();
+
+console.log( newSingleton1.random );
+console.log( newSingleton2.random );
+
+newSingleton1.random = 7;
+
+console.log( newSingleton1.random );
+console.log( newSingleton2.random );
