abfd5869daf72467d51ba996cd399564b105470d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-26 08:26:09 -0500

    strategy parte 2

--- strategy/js/index.js
+++ strategy/js/index.js
@@ -4,6 +4,10 @@ class SalesContext {
         this.strategy = strategy;
     }
 
+    getStrategy(strategy){
+        this.strategy = strategy;
+    }
+
     calculate( amount ){
         return this.strategy.calculate(amount);
     }
@@ -22,9 +26,42 @@ class RegularSalesStrategy{
 }
 
 
-const regularSale = new RegularSalesStrategy(0.16)
+class DiscountSaleStrategy{
+    constructor( tax, discount ){
+        this.tax = tax;
+        this.discount = discount;
+    }
 
-const sale = new SalesContext(regularSale);
+    calculate(amount){
+        return (amount + ( amount * this.tax )) - this.discount;
+    }
+}
 
 
-console.log(sale.calculate( 18 ));
+class ForeingSaleStrategy{
+
+    getDollar(){
+        return 20;
+    }
+
+    calculate(amount){
+        return amount * this.getDollar();
+    }
+}
+
+const regularSale = new RegularSalesStrategy(0.16);
+const discountSale = new DiscountSaleStrategy(0.16,3);
+const  foreingSale = new ForeingSaleStrategy();
+
+const sale1 = new SalesContext(regularSale);
+const sale2 = new SalesContext(discountSale);
+
+
+
+console.log(sale1.calculate( 18 ));
+console.log(sale2.calculate( 18 ));
+
+sale2.getStrategy(foreingSale);
+
+
+console.log(sale2.calculate( 18 ));
