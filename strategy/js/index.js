ad67f1a37b397e66092ab36fa8d89a75c75df66b
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-26 08:49:48 -0500

    strategy parte 3

--- strategy/js/index.js
+++ strategy/js/index.js
@@ -65,3 +65,109 @@ sale2.getStrategy(foreingSale);
 
 
 console.log(sale2.calculate( 18 ));
+
+const data = [{
+    name: "Erdinger Pikantus",
+    country: "Alemania",
+    info: "Erdinger Pikantus es una cerveza de estilo weizenbock elaborada en la localidad bávara de Erding.",
+    img: "https://dxjcdxuv6chk2.cloudfront.net/assets/biere/flascheglas/pikantus-2020-v2.png"
+},
+{
+    name: "Corona",
+    country: "México",
+    info: "La cerveza Corona es una marca mundialmente conocida, distribuida a lo largo de más de 159 países en los cinco continentes.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Corona-6Pack.JPG"
+},
+{
+    name: "Delirium Tremens",
+    country: "Bélgica",
+    info: "Esta pale ale tiene una efervescencia fina con un toque un tanto picante. Al tomarse, calienta el paladar y deja un sabor fuerte y de un amargor seco.",
+    img: "https://www.delirium.be/themes/custom/delirium/assets/img/beers/beer_delirium_tremens_bottle.png"
+}];
+
+
+class InfoContext{
+
+    constructor(strategy, data, element){
+        this.setStrategy(strategy);
+        this.data = data;
+        this.element = element;
+    }
+
+    setStrategy(strategy){
+        this.strategy = strategy;
+    }
+
+    show(){
+        this.strategy.show(this.data, this.element);
+    }
+}
+
+class ListStrategy{
+
+    show(data, element){
+
+        element.innerHTML = data.reduce((ac, e)=>{
+            
+            return ac + `<div>
+                    <h2>${e.name}</h2> 
+                    <p>${e.country}<p>
+                </div>
+            <hr>`;
+        }, "");
+
+    }
+}
+
+class DetailedListStrategy{
+
+    show(data, element){
+        element.innerHTML = data.reduce((ac, e)=>{
+    
+            return ac + `<div>
+                    <h2>${e.name}</h2> 
+                    <p>${e.country}<p>
+                    <p>${e.info}</p>
+                </div>
+            <hr>`;
+        }, "");
+
+    }
+}
+
+class ListWithImageStrategy{
+
+    show(data, element){
+        element.innerHTML = data.reduce((ac, e)=>{
+                return ac + `<div>
+                        <img width="10%" src="${e.img}">
+                        <h2>${e.name.toUpperCase()}</h2> 
+                        <p>${e.country}<p>
+                        <p>${e.info}</p>
+                    </div>
+                <hr>`;
+        }, "");
+
+    }
+}
+
+
+
+const strategies = [
+    new ListStrategy(),
+    new DetailedListStrategy(),
+    new ListWithImageStrategy()
+]
+
+const info = new InfoContext(
+    new ListStrategy(),
+    data,
+    content);
+
+info.show();
+
+slcOptions.addEventListener("change",(event)=>{
+    const op = event.target.value;
+    info.setStrategy(strategies[op]);
+    info.show();
+});
