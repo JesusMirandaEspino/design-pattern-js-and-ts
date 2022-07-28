6cb407283d3aa99ec208b949772e4c69055024aa
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-28 08:28:47 -0500

    observer part2

--- observer/js/index.js
+++ observer/js/index.js
@@ -1,20 +1,43 @@
-
 class Subject{
     constructor(){
         this.observers = [];
     }
 
-    subscribe(){
-        this.observers.push();
+    subscribe(observer){
+        this.observers.push(observer);
     }
 
     unsubscribe(observer){
-        this.observers = this.observers.filter( obs => obs !== observer );
+        this.observers = this.observers.filter(obs => obs !== observer);
     }
 
     notify(data){
-        this.observers.forEach( obs => {
-            obs.refresh(data)
+        this.observers.forEach(e=>{
+            e.refresh(data);
         });
     }
+}
+class Observer{
+    
+    constructor(fn){
+        this.fn = fn
+    }
+
+    refresh(data){
+        this.fn(data);
+    }
+}
+
+
+const s = new Subject();
+const o1= new Observer((d)=>console.log("Hola soy el observador 1 "+d));
+
+
+
+s.subscribe(o1);
+
+
+
+function change(){
+    s.notify(myText.value);
 }
