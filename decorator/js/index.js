4ca8de1d300d83a9f77ff9a54cfd850c1897e9ee
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-08-02 08:56:53 -0500

    decorator part2

--- decorator/js/index.js
+++ decorator/js/index.js
@@ -38,3 +38,54 @@ class CommercialInfoProductDecorator extends ProductDecorator{
                     super.getDetail();
     }
 }
+
+
+// decorator 2
+class StoreProductDecorator extends ProductDecorator{
+
+    constructor(productComponent, price){
+        super(productComponent);
+        
+        this.price = price;
+        
+    }
+
+    getDetail(){
+        return super.getDetail() +
+                ` $ ${this.price}`;
+    }
+}
+
+// decorator 3
+class HTMLProductDecorator extends ProductDecorator{
+
+    getDetail(){
+        return `<h1>Información del producto</h1>
+                <p>
+                    ${super.getDetail()}
+                </p>`;
+    }
+}
+
+
+// Ejecución
+// component
+const productComponent = new ProductComponent("Cerveza");
+console.log(productComponent.getDetail());
+
+// decorator 1 con component
+const commercialInfoProduct = 
+    new CommercialInfoProductDecorator(productComponent, "London Porter", "Fuller's");
+console.log(commercialInfoProduct.getDetail());
+
+// decorator 2 con component
+const storeProduct = new StoreProductDecorator(productComponent, 15.5);
+console.log(storeProduct.getDetail());
+
+// decorator 2 con decorator 1
+const product = new StoreProductDecorator(commercialInfoProduct, 15.5);
+console.log(product.getDetail());
+
+// decorator 3 con decorator 2 con decorator 1
+const htmlProductDecorator = new HTMLProductDecorator(product);
+myDiv.innerHTML = htmlProductDecorator.getDetail();
